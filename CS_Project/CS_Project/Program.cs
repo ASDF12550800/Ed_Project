8ff965d3fe25dabb4c62c44085728124dd597661
ASDF12550800 <109446750+ASDF12550800@users.noreply.github.com>
2022-07-31 11:46:01 +0900

    Add files via upload

---
+++ CS_Project/CS_Project/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CS_Project
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World\n\n");
+
+            int i1 = 3;
+            string s1 = "Hi";
+
+            Console.WriteLine(i1 + " " + s1);
+            Console.WriteLine("{0} {1}", i1, s1);
+            Console.WriteLine($"{i1} {s1}");
+            Console.ReadKey();
+        }
+    }
+}
